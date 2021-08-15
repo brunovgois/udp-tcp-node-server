41587b43abfa9281558958be72ab23e48003768a
Bruno Gois <brunovgois@gmail.com>
2021-08-15 00:29:19 -0300

    boilerplate

---
+++ client.js
@@ -0,0 +1,14 @@
+//client 
+// const axios = require('axios')
+
+// axios
+//   .post('TODO', {
+//     message: 'to be capitalized'
+//   })
+//   .then(res => {
+//     console.log(`statusCode: ${res.status}`)
+//     console.log(res)
+//   })
+//   .catch(error => {
+//     console.error(error)
+//   })
